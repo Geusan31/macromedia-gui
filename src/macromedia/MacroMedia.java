539dc1cba2f05051ef2ee955bafe8e7679fa9d8f
Geusan31 <edurais.ariadaffa@gmail.com>
2025-03-12 12:36:41 +0700

    first commit

---
+++ src/macromedia/MacroMedia.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package macromedia;
+
+/**
+ *
+ * @author Helios
+ */
+public class MacroMedia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
